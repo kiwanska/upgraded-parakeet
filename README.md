cc6b13d945c48f39de55b1a00b1403e0384c456b
Kasia Iwańska <katarzyna.iwanska@netguru.co>
2018-06-07 08:54:02 +1000

    :tada: Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # upgraded-parakeet
+
+yarn install
+yarn start
---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="parakeet">
+        <header className="parakeet__header">
+          <h1 className="parakeet__title">🥝 🐸 🌵</h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
